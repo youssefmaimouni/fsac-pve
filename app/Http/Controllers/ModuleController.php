d0573d51fdd4502f6a2b5575dabb52ba7ea70c06
youssef Maimouni <youssefmaimouni03@gmail.com>
2024-04-29 21:10:34 +0100

    module et filiere implementation

---
+++ app/Http/Controllers/ModuleController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\CreateModuleRequest;
+use App\Http\Requests\EditModuleRequest;
+use App\Models\module;
+use Exception;
+use Illuminate\Http\Request;
+
+class ModuleController extends Controller
+{
+    public function index(){
+        return 'Liste des article';
+    }
+    public function store(CreateModuleRequest $request){
+
+        try{
+        $module = new module();
+        $module->intitule_module=$request->intitule_module;  
+        $module->id_filiere=$request->id_filiere;
+
+        $module->save();
+
+
+        return response()->json([
+            'status_code'=>201,
+            'status_message'=>'le post a été ajouté',
+            'data'=>$module
+        ]);
+        
+        }catch(Exception $exeption){
+            return response()->json($exeption);
+        }
+        
+    }
+
+    public function update(EditModuleRequest $request,module $module) {
+        
+
+        try{
+        
+        $module->intitule_module=$request->intitule_module;
+        $module->id_filiere=$request->id_filiere;
+      
+        $module->save();
+
+        return response()->json([
+            'status_code'=>201,
+            'status_message'=>'la module  a été modifié',
+            'data'=>$module
+        ]);
+
+        }catch(Exception $exeption){
+            return response()->json($exeption);
+        }
+       
+    }
+
+    public function delete(module $module) {
+         try{
+                $module->delete();
+
+            return response()->json([
+                'status_code'=>200,
+                'status_message'=>'la module  a été supprimer',
+                'data'=>$module
+            ]);
+            
+            
+         }catch(Exception $exeption){
+            return response()->json($exeption);
+        }
+    }
+}
---
+++ app/Http/Requests/CreateModuleRequest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class CreateModuleRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'intitule_module'=>'required',
+            'id_filiere'=>'required'
+        ];
+    }
+
+    public function failedValidation(Validator $validator){
+        throw new HttpResponseException(response()->json([
+            'success'=>false,
+            'error'=>true,
+            'message'=> 'Erreur de validation',
+            'errorList'=>$validator->errors(),
+
+        ]));
+    }
+    public function messages()
+    {
+        return[
+            'intitule_module.required'=>'une intitule de module doit etre fourni',
+            'id_filiere.required'=>'le id de filiére doit etre fourni'
+
+        ];
+    }
+}
