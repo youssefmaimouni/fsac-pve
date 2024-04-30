e32e9659babc0d689b7f3a5bd9883727dc7806eb
youssef Maimouni <youssefmaimouni03@gmail.com>
2024-04-30 23:27:03 +0100

    Rapport et PV

---
+++ database/migrations/2024_04_30_175340_create_pvs_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pvs', function (Blueprint $table) {
+            $table->bigIncrements('id_pv');
+            $table->unsignedBigInteger('id_tablette');
+            $table->foreign('id_tablette')->references('id_tablette')->on('tablettes');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pvs');
+    }
+};
