558bb965572a5ea975e52c27217bc12f47953e3e
youssef Maimouni <youssefmaimouni03@gmail.com>
2024-04-30 23:41:09 +0100

    Rapport et pv

--- app/Models/Rapport.php
+++ app/Models/Rapport.php
@@ -7,5 +7,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Rapport extends Model
 {
-    use HasFactory;
+    protected $primaryKey = 'id_rapport';
+
+    protected $fillable =[
+        'titre_rapport',
+        'contenu',
+        'id_pv',
+        'codeApogee',
+    ];
+
+    public function PV()
+    {
+        return $this->belongsTo(pv::class, 'id_pv');
+    }
+    public function etudiant()
+    {
+        return $this->belongsTo(etudiant::class, 'codeApogee');
+    }
 }
--- app/Models/pv.php
+++ app/Models/pv.php
@@ -7,5 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class pv extends Model
 {
-    use HasFactory;
+    protected $primaryKey = 'id_pv';
+
+    protected $fillable =[
+        'id_tablette'
+    ];
+    // public function tablette(){
+    //     return $this->belongsTo(tablette::class,'id_tablette');
+    // }
 }
