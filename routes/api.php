adaab609c26e9535463e29f33e879adb1fc4a878
youssef Maimouni <youssefmaimouni03@gmail.com>
2024-04-30 23:30:52 +0100

    Rapport et PV

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\etudiantController;
 use App\Http\Controllers\FiliereController;
 use App\Http\Controllers\ModuleController;
+use App\Http\Controllers\PVController;
+use App\Http\Controllers\RapportController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -35,6 +37,15 @@ Route::get('etudiant',[etudiantController::class,'index']);
 Route::post('etudiant/create',[etudiantController::class,'store']);
 Route::put('etudiant/edit/{etudiant}',[etudiantController::class,'update']);
 Route::delete('etudiant/{etudiant}',[etudiantController::class,'delete']);
+
+
+Route::post('pv/create',[PVController::class,'store']);
+Route::put('pv/edit/{pv}',[PVController::class,'update']);
+Route::delete('pv/{pv}',[PVController::class,'delete']);
+
+Route::post('rapport/create',[RapportController::class,'store']);
+Route::put('rapport/edit/{rapport}',[RapportController::class,'update']);
+Route::delete('rapport/{rapport}',[RapportController::class,'delete']);
  
 
 
