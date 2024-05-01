edf7a46f3f048c700d72c81bdfceab9ca678efe8
AyoubHi <elmustafaayoub@gmail.com>
2024-05-01 13:32:49 +0100

    l ajout des departement et surveillant

---
+++ app/Http/Requests/EditDepartementRequest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class EditDepartementRequest extends FormRequest
+{
+   /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'id_departement'=>'required | unique:departements'
+        ];
+    }
+
+    public function failedValidation(Validator $validator){
+        throw new HttpResponseException(response()->json([
+            'success'=>false,
+            'error'=>true,
+            'message'=> 'Erreur de validation',
+            'errorList'=>$validator->errors(),
+
+        ]));
+    }
+    public function messages()
+    {
+        return[
+            'id_departement.required'=>'un nom de departement doit etre fourni',
+            'id_departement.unique'=>'un nom de departement doit etre unique'
+        ];
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,13 @@
 <?php
 
 use App\Http\Controllers\etudiantController;
+use App\Http\Controllers\surveillantController;
+use App\Http\Controllers\departementController;
 use App\Http\Controllers\FiliereController;
 use App\Http\Controllers\ModuleController;
 use App\Http\Controllers\PVController;
 use App\Http\Controllers\RapportController;
+use App\Models\departement;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -39,6 +42,13 @@ Route::put('etudiant/edit/{etudiant}',[etudiantController::class,'update']);
 Route::delete('etudiant/{etudiant}',[etudiantController::class,'delete']);
 
 
+Route::get('surveillant',[surveillantController::class,'index']);
+
+Route::post('surveillant/create',[surveillantController::class,'store']);
+Route::put('surveillant/edit/{surveillant}',[surveillantController::class,'update']);
+Route::delete('surveillant/{surveillant}',[surveillantController::class,'delete']);
+
+
 Route::post('pv/create',[PVController::class,'store']);
 Route::put('pv/edit/{pv}',[PVController::class,'update']);
 Route::delete('pv/{pv}',[PVController::class,'delete']);
@@ -46,6 +56,14 @@ Route::delete('pv/{pv}',[PVController::class,'delete']);
 Route::post('rapport/create',[RapportController::class,'store']);
 Route::put('rapport/edit/{rapport}',[RapportController::class,'update']);
 Route::delete('rapport/{rapport}',[RapportController::class,'delete']);
+
+
+
+Route::get('departement',[departementController::class,'index']);
+
+Route::post('departement/create',[departementController::class,'store']);
+Route::put('departement/edit/{departement}',[departementController::class,'update']);
+Route::delete('departement/{departement}',[departementController::class,'delete']);
  
 
 
