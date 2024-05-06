c80dd014bb5b34d6911cb0b6cf917ef03c68164c
youssef Maimouni <youssefmaimouni03@gmail.com>
2024-05-06 11:53:21 +0100

    admin try catch

--- app/Http/Controllers/administrateurcontroller.php
+++ app/Http/Controllers/administrateurcontroller.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\administrateurRequest;
 use App\Models\administrateur;
 use Exception;
-
+use PhpParser\Node\Stmt\TryCatch;
 
 class administrateurController extends Controller
 {
@@ -36,12 +36,22 @@ class administrateurController extends Controller
 
     public function update(administrateurRequest $request,administrateur $administrateur )
     {
-        $administrateur->mail=$request->mail;
+        try{
+            $administrateur->mail=$request->mail;
         $administrateur->nom=$request->nom;
         $administrateur->prenom=$request->prenom;
         $administrateur->mot_de_passe = bcrypt($request->mot_de_passe);
         $administrateur->save();
+        return response()->json([
+            'status_code'=>200,
+            'status_message'=>'l\'administrateur a été modifier',
+            'data'=>$administrateur
+        ]);
+        }catch(Exception $exception){
+            return response()->json($exception);
         }
+        
+    }
 
 public function delete(administrateur  $administrateur) {
          try{
