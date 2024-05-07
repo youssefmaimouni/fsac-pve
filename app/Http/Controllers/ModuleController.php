e0e69abc5ba07b1dfa954a2435a01c9b7527371b
ihssane imouchi <ihssane.imouchi-etu@etu.univh2c.ma>
2024-05-07 10:00:45 +0100

    modif

--- app/Http/Controllers/ModuleController.php
+++ app/Http/Controllers/ModuleController.php
@@ -11,9 +11,81 @@ use Illuminate\Http\Request;
 
 class ModuleController extends Controller
 {
+    /**
+     * @OA\Get(
+     *     path="/api/module",
+     *     tags={"module"},
+     *     summary="Get all modules for REST API",
+     *     description="Multiple status values can be provided with comma separated string",
+     *     operationId="moduleindex",
+     *     @OA\Parameter(
+     *         name="status",
+     *         in="query",
+     *         description="Status values that needed to be considered for filter",
+     *         required=true,
+     *         explode=true,
+     *         @OA\Schema(
+     *             default="available",
+     *             type="string",
+     *             enum={"available", "pending", "sold"},
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="successful operation",
+     *     ),
+     *     @OA\Response(
+     *         response=400,
+     *         description="Invalid status value"
+     *     ),
+     * )
+     */
     public function index(){
-        return 'Liste des modules';
+         $modules = module::all();
+         return $modules;
     }
+     /**
+     * @OA\Post(
+     *     path="/api/module/create",
+     *     tags={"module"},
+     *     summary="create all modules for REST API",
+     *     description="Multiple status values can be provided with comma separated string",
+     *     operationId="modulestore",
+     *     @OA\Parameter(
+     *         name="status",
+     *         in="query",
+     *         description="Status values that needed to be considered for filter",
+     *         required=true,
+     *         explode=true,
+     *         @OA\Schema(
+     *             default="available",
+     *             type="string",
+     *             enum={"available", "pending", "sold"},
+     *         )
+     *     ),
+     *      @OA\RequestBody(
+     *         description="Book data that needs to be added to the store",
+     *         required=true,
+     *         @OA\JsonContent(
+     *            
+     *            @OA\Property(property="id_filiere", type="integer", example=""),
+     *            @OA\Property(property="intitule_module", type="string", example="")
+     *             
+     *             
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="successful operation",
+     *         @OA\JsonContent() 
+     *     ),
+     *     @OA\Response(
+     *         response=400,
+     *         description="Invalid status value"
+     *     ),
+     * )
+     */
+
     public function store(CreateModuleRequest $request){
 
         try{
@@ -35,7 +107,41 @@ class ModuleController extends Controller
         }
         
     }
-
+    /**
+     * @OA\Put(
+     *     path="/api/module/edit/{module}",
+     *     tags={"module"},
+     *     summary="update all modules for REST API",
+     *     description="Multiple status values can be provided with comma separated string",
+     *     operationId="moduleupdate",
+     *    @OA\Parameter(
+     *          name="module",
+     *          description="module id",
+     *          required=true,
+     *          in="path",
+     *          @OA\Schema(
+     *              type="integer"
+     *          )
+     *      ),
+     *      @OA\RequestBody(
+     *         description="Book data that needs to be added to the store",
+     *         required=true,
+     *         @OA\JsonContent(
+     *             @OA\Property(property="id_filiere", type="integer", example=""),
+     *            @OA\Property(property="intitule_module", type="string", example="")
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="successful operation",
+     *         @OA\JsonContent() 
+     *     ),
+     *     @OA\Response(
+     *         response=400,
+     *         description="Invalid status value"
+     *     ),
+     * )
+     */
     public function update(EditModuleRequest $request,module $module) {
         
 
@@ -57,6 +163,33 @@ class ModuleController extends Controller
         }
        
     }
+    /**
+     * @OA\Delete(
+     *     path="/api/module/{module}",
+     *     tags={"module"},
+     *     summary="delete all modules for REST API",
+     *     description="Multiple status values can be provided with comma separated string",
+     *     operationId="moduledelete",
+     *     @OA\Parameter(
+     *          name="module",
+     *          description="module id",
+     *          required=true,
+     *          in="path",
+     *          @OA\Schema(
+     *              type="integer"
+     *          )
+     *      ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="successful operation",
+     *         @OA\JsonContent() 
+     *     ),
+     *     @OA\Response(
+     *         response=400,
+     *         description="Invalid status value"
+     *     ),
+     * )
+     */
 
     public function delete(module $module) {
          try{
